4e6d1ed5656c20a2b7e70ba2851b670b9f777dc5
Каневская Елизавета Юрьевна <lizok.kanev@mail.ru>
2025-10-19 08:35:24 +0300

    Create class Hospital.py

---
+++ program.py
@@ -0,0 +1,10 @@
+class Hospital:
+  def __init__(self, name , adress, guide):
+    self.name = name
+    self.adress = adress
+    self.guide = guide #руководство
+  def add_patient():
+    ...
+  def add_doctor():
+    ...
+  
