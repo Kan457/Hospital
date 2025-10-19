e4da47b0a09beb33081155cda60da6d2a1658e42
Каневская Елизавета Юрьевна <lizok.kanev@mail.ru>
2025-10-20 01:22:56 +0300

    Update program guide and hospital.py

--- program.py
+++ program.py
@@ -1,24 +1,61 @@
+import json
+class Guide:
+  def __init__(self,chief_medical,phone,mail):
+    self.chief_medical = chief_medical
+    self.phone = phone
+    self.mail = mail
+  def set(self,chief_medical,phone,mail):
+    self.chief_medical = chief_medical
+    self.phone = phone
+    self.mail = mail 
+  def guid_dict(self):
+    return {
+      'Главный врая' : self.chief_medical,
+      'Телефон': self.phone,
+      'mail.ru': self.mail
+    }
+
 class Hospital:
-  def __init__(self, name , adress, guide):
+  def __init__(self, name , adress, guide , to_dict):
     self.name = name
     self.adress = adress
-    self.guide = guide #руководство
-  def add_patient():
-    ...
-  def add_doctor():
-    ...
-  def Json_add(object): #добавление обекта в json
+    self.guide = Guide()
+  #для более корректного получения значений
+  def set(self,name,adress,guide):
+    self.name = name
+    self.adress = adress
+    self.guide = Guide()
+  #словарь для работы с json
+  def to_dict(self):
+    return {
+      'Название' : self.name,
+      'Адресс': self.adress,
+      'Руководство': self.guide.guide_dict()
+    }
+  
+  #добавление обекта в json
+  def Json_add(object): 
     json_data = json.dumps(object.__dict__, ensure_ascii=False)
     return json_data
     
 class Administration:
-  def __init__(self,chief_medical,telephone,mail,hospital):#агрегация больница - администрация
+  def __init__(self,chief_medical,telephone,mail,hospital):#Компазиция больница - администрация
     self.chief_medical = chief_medical
     self.telephone = telephone
     self.mail = mail
   def Biography():
     ...
-  def DF_doctor()
-    
+  def DF_doctor():
+    ...
+class Doctor:#
+  def __init__(self,name,specialty, work_experience):
+    self.name = name
+    self.specialty = specialty
+    self.work_experience = work_experience
+  def retuer():
+    ...
+
+
+Hospital1 = Hospital('Поликлиника 109','ул.Пушкинская д.12','Петрова А.Н')
     
   
