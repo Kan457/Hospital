a1ee2d450c13a878d9322c38cf4565d79d589441
Каневская Елизавета Юрьевна <lizok.kanev@mail.ru>
2025-10-20 20:43:40 +0300

    Update hospital2.py

--- program.py
+++ program.py
@@ -1,6 +1,37 @@
 import json
-class Guide:
-  def __init__(self,chief_medical,phone,mail):
+#------------------Больница-------------------1
+class Hospital:
+  def __init__(self, name : str , adress : str, 
+               staff : Staff , doctor : Doctor, patient : Patient, 
+               attachment : Attachment, diagnosis : Diagnosis, work_schedule : Work_schedule, 
+               referral : Referral, tests : Tests, medical_record : Medical_record, record : Record):
+  self.name = name
+  self.adress = adress
+  self.staff = sraff
+  self.doctor = doctor
+  self.patient = patient
+  self.attachment = attachment
+  self.diagnosis = diagnosis
+  self.work_schedule = work_schedule
+  self.referral = Referral
+  self.tests = tests
+  self.medical_record = medical_record
+  self.record = record
+  #Корректное получение значений
+  def set(self,name,adress,guide):
+    self.name = name
+    self.adress = adress
+    self.staff = sraff
+  #Словарь Поликлиника
+  def to_dict(self):
+    return {
+      'Название' : self.name,
+      'Адресс': self.adress,
+      'Руководство': self.guide.guide_dict()
+    }
+#------------------Персонал-------------------2
+class Staff:
+  def __init__(self , chief_medical : string , phone : string , mail : string):
     self.chief_medical = chief_medical
     self.phone = phone
     self.mail = mail
@@ -14,39 +45,7 @@ class Guide:
       'Телефон': self.phone,
       'mail.ru': self.mail
     }
-
-class Hospital:
-  def __init__(self, name , adress, guide , to_dict):
-    self.name = name
-    self.adress = adress
-    self.guide = Guide()
-  #для более корректного получения значений
-  def set(self,name,adress,guide):
-    self.name = name
-    self.adress = adress
-    self.guide = Guide()
-  #словарь для работы с json
-  def to_dict(self):
-    return {
-      'Название' : self.name,
-      'Адресс': self.adress,
-      'Руководство': self.guide.guide_dict()
-    }
-  
-  #добавление обекта в json
-  def Json_add(object): 
-    json_data = json.dumps(object.__dict__, ensure_ascii=False)
-    return json_data
-    
-class Administration:
-  def __init__(self,chief_medical,telephone,mail,hospital):#Компазиция больница - администрация
-    self.chief_medical = chief_medical
-    self.telephone = telephone
-    self.mail = mail
-  def Biography():
-    ...
-  def DF_doctor():
-    ...
+#------------------Врачи-------------------3  
 class Doctor:#
   def __init__(self,name,specialty, work_experience):
     self.name = name
@@ -54,8 +53,14 @@ class Doctor:#
     self.work_experience = work_experience
   def retuer():
     ...
-
-
+#------------------Пациенты-------------------3
+#------------------Прикрепление-------------------5
+#------------------Диагноз-------------------6
+#------------------График работы-------------------7
+#------------------Направление-------------------8
+#------------------Анализы-------------------9
+#------------------Медицинская карта-------------------10
+#------------------Запись-------------------11
 Hospital1 = Hospital('Поликлиника 109','ул.Пушкинская д.12','Петрова А.Н')
     
   
